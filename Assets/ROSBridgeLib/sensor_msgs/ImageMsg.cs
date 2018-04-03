e027c3806ce25832abe85bf8deaf28b66219b869
Johnfred Cornell <johnyiestfred@yahoo.com>
2018-04-02 17:35:01 -0700

    initial commit

---
+++ Assets/ROSBridgeLib/sensor_msgs/ImageMsg.cs
@@ -0,0 +1,97 @@
+﻿using LiveFeedScreen.ROSBridgeLib;
+using LiveFeedScreen.ROSBridgeLib.std_msgs.std_msgs;
+using Tobii.Plugins;
+
+/**
+ * Define a Image message.
+ *  
+ * @author Mathias Ciarlo Thorstensen
+ */
+
+namespace LiveFeedScreen.ROSBridgeLib.sensor_msgs
+{
+    namespace sensor_msgs
+    {
+        public class ImageMsg : ROSBridgeMsg
+        {
+            private readonly HeaderMsg _header;
+            private readonly uint _height;
+            private readonly uint _width;
+            private readonly string _encoding;
+            private readonly bool _is_bigendian;
+            private readonly uint _row_step;
+            private readonly byte[] _data;
+
+            public ImageMsg(JSONNode msg)
+            {
+                _header = new HeaderMsg(msg["header"]);
+                _height = uint.Parse(msg["height"]);
+                _width = uint.Parse(msg["width"]);
+                _encoding = msg["encoding"];
+                _is_bigendian = msg["is_bigendian"].AsBool;
+                _row_step = uint.Parse(msg["step"]);
+                _data = System.Convert.FromBase64String(msg["data"]);
+            }
+
+            public ImageMsg(HeaderMsg header, uint height, uint width, string encoding, bool is_bigendian, uint row_step, byte[] data)
+            {
+                _header = header;
+                _height = height;
+                _width = width;
+                _encoding = encoding;
+                _is_bigendian = is_bigendian;
+                _row_step = row_step;
+            }
+
+            public HeaderMsg GetHeader()
+            {
+                return _header;
+            }
+
+            public uint GetWidth()
+            {
+                return _width;
+            }
+
+            public uint GetHeight()
+            {
+                return _height;
+            }
+
+            public uint GetRowStep()
+            {
+                return _row_step;
+            }
+
+            public byte[] GetImage()
+            {
+                return _data;
+            }
+
+            public static string GetMessageType()
+            {
+                return "sensor_msgs/Image";
+            }
+
+            public override string ToString()
+            {
+                return "Image [header=" + _header.ToString() +
+                    "height=" + _height +
+                    "width=" + _width +
+                    "encoding=" + _encoding +
+                    "is_bigendian=" + _is_bigendian +
+                    "row_step=" + _row_step + "]";
+            }
+
+            public override string ToYAMLString()
+            {
+                return "{\"header\" :" + _header.ToYAMLString() +
+                    "\"height\" :" + _height +
+                    "\"width\" :" + _width +
+                    "\"encoding\" :" + _encoding +
+                    "\"is_bigendian\" :" + _is_bigendian +
+                    "\"row_step\" :" + _row_step + "}";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FeedScreen/ImageSubscriber.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LiveFeedScreen.ROSBridgeLib;
+using LiveFeedScreen.ROSBridgeLib.sensor_msgs.sensor_msgs;
+using Tobii.Plugins;
+
+public class ImageSubscriber : MonoBehaviour
+{
+
+    private RawImage rawimg;
+    private static Texture2D texture;
+
+    public int RobotID;
+    private static int ID;
+
+    private void Start()
+    {
+        rawimg = GetComponent<RawImage>();
+        texture = new Texture2D(2, 2);
+
+        ID = RobotID;
+    }
+    private void Update()
+    {
+        rawimg.texture = texture;
+    }
+    public new static string GetMessageTopic()
+    {
+        //return "/robot1/camera/rgb/image_raw/compressed";
+        return "/robot4/camera/rgb/image_boxed/compressed";
+
+        // return string.Format("/robot{0}/camera/rgb/image_raw/compressed", ID);
+    }
+
+    public new static string GetMessageType()
+    {
+        // return "sensor_msgs/ImageSubscriber";
+        return "sensor_msgs/Image";
+    }
+
+    // Important function (I think, converting json to PoseMsg)
+    public new static ROSBridgeMsg ParseMessage(JSONNode msg)
+    {
+        Debug.Log("ParseMessage in ImageSubscriber---------MIGUEL");
+        return new ImageMsg(msg);
+    }
+
+    // This function should fire on each ros message
+    public new static void CallBack(ROSBridgeMsg msg)
+    {
+        Debug.Log(GetMessageTopic() + " received");
+        ImageMsg imageMsg = (ImageMsg)msg;
+        Debug.Log("HERE IS THE BYTE[] COMMING IN FROM ROS");
+        Debug.Log("------------------------------------------");
+        byte[] arr;
+        arr = imageMsg.GetImage();
+        Debug.Log(imageMsg.GetImage());
+        Debug.Log(arr[1]);
+        Debug.Log("------------------------------------------");
+
+        byte[] bimage = imageMsg.GetImage();
+        texture.LoadImage(bimage);
+        //texture.LoadRawTextureData(bimage);
+        texture.Apply();
+
+
+        // GUI.DrawTexture(new Rect(10, 10, 60, 60), texture, ScaleMode.ScaleToFit, true, 10.0F);
+        //GetComponent<Renderer>().material.mainTexture = texture;
+    }
+}
