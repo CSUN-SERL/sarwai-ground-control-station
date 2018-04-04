1732b7272593933b2676b88fe38c955c5bdda319
Anton Velikodnyy <anton.velikodnyy.779@my.csun.edu>
2018-04-04 14:45:41 -0700

    added a way to continue if no trasparency

--- Assets/Scripts/TransparencyIrisToOperator/TransparencyBrief.cs
+++ Assets/Scripts/TransparencyIrisToOperator/TransparencyBrief.cs
@@ -234,8 +234,8 @@ namespace TransparencyIrisToOperator
                     else if (result["data"].Count() == 0)
                     {
                         Debug.Log("Empty Transparency");
+                        GoToNextScene();
                         yield return null;
-                        //GoToNextScene();
                     }
 
                     foreach (var query in result["data"])
