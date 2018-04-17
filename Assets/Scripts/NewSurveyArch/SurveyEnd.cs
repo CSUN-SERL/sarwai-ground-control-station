2708cf15ff51eb7e145846ab8f959bffbadb03d1
Collin Emerson Miller <Collin.Emerson.Miller@gmail.com>
2018-04-17 01:54:36 -0700

    added history back for the 500th time

--- Assets/Scripts/NewSurveyArch/SurveyEnd.cs
+++ Assets/Scripts/NewSurveyArch/SurveyEnd.cs
@@ -9,14 +9,18 @@ namespace NewSurveyArch
     /// </summary>
     public class SurveyEnd : MonoBehaviour
     {
+        private int events;
+
         public void OnEnable()
         {
-            EventManager.PushedSurvey += OnEnd;
+            EventManager.PushedSurvey += OnPushed;
+            EventManager.SurveyComplete += OnComplete;
         }
 
         public void OnDisable()
         {
-            EventManager.PushedSurvey -= OnEnd;
+            EventManager.PushedSurvey -= OnPushed;
+            EventManager.SurveyComplete -= OnComplete;
         }
 
         /// <summary>
@@ -29,18 +33,42 @@ namespace NewSurveyArch
                 Instance = this;
             else if (Instance != this)
                 Destroy(gameObject);
-                
+            events = 0;
+
         }
 
-
         /// <summary>
-        ///     Ends the scene
+        ///     Marks pushed as true
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private static void OnEnd(object sender, EventArgs e)
+        private void OnPushed(object sender, EventArgs e)
         {
-            SceneFlowController.LoadNextScene();
+            Debug.Log("Survey was recognized as pushed by SurveyEnd");
+            Done();
         }
+
+        /// <summary>
+        ///     Marks pushed as true
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnComplete(object sender, EventArgs e)
+        {
+            Debug.Log("Survey was recognized as complete by SurveyEnd");
+            Done();
+        }
+
+        private void Done()
+        {
+            ++events;
+            if (events == 2)
+            {
+                Debug.Log("Survey was ended by SurveyEnd");
+                SceneFlowController.LoadNextScene();
+            }
+
+        }
+
     }
 }
