c38c4135131568f07935f50396be899c921d04d8
MaxConners <MaxConers@gmail.com>
2018-04-23 16:45:51 -0700

    added documentation for scene flow controller and GCS Socket.

--- Assets/Scripts/FeedScreen/Experiment/SceneFlowController.cs
+++ Assets/Scripts/FeedScreen/Experiment/SceneFlowController.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 namespace FeedScreen.Experiment
 {
+    /// <summary>
+    /// Scene flow controller controls the transitions from scene to scene in the ground control station.
+    /// This class follows the singleton design pattern.
+    /// </summary>
     internal class SceneFlowController : MonoBehaviour
     {
+
+        // Names of all of the scenes.
         public const string ProctorSetup = "ProctorSetup";
         public const string Welcome = "Welcome";
         public const string GeneralSurvey = "NewSurvey";
@@ -13,8 +19,32 @@ namespace FeedScreen.Experiment
         public const string FinalScene = "FinalScene";
         public const string Error = "Error";
 
+
+        // Instance for the singleton.
         public static SceneFlowController Instance;
 
+        /// <summary>
+        /// Makes sure that there is only one instance of this object at a time.
+        /// </summary>
+        private void Awake()
+        {
+            if (Instance == null) Instance = this;
+            else if (Instance != this)
+                Destroy(gameObject);
+        }
+
+        /// <summary>
+        /// Makes sure that the scene flow controller does not get deleted when moving from scene to scene.
+        /// </summary>
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+
+        /// <summary>
+        /// Given the current state of the ground control station, transition to the next scene.
+        /// </summary>
         public static void LoadNextScene()
         {
             var scene = SceneManager.GetActiveScene();
@@ -49,26 +79,22 @@ namespace FeedScreen.Experiment
             }
         }
 
+
+        /// <summary>
+        /// The error scene is a catch-all for all critical system errors that are non recoverable.
+        /// </summary>
         public static void LoadErrorScene()
         {
             SceneManager.LoadScene(Participant.Participant.Error);
         }
 
+
+        /// <summary>
+        /// This is a wrapper for gameobjects in unity to go to the next mission.
+        /// </summary>
         public void LoadNextSceneWrapper()
         {
             LoadNextScene();
-        }
-
-        private void Awake()
-        {
-            if (Instance == null) Instance = this;
-            else if (Instance != this)
-                Destroy(gameObject);
-        }
-
-        private void Start()
-        {
-            DontDestroyOnLoad(gameObject);
         }
     }
 }
--- Assets/Scripts/Participant/NewParticipantEventArgs.cs
+++ Assets/Scripts/Participant/NewParticipantEventArgs.cs
@@ -2,6 +2,10 @@
 
 namespace Participant
 {
+
+    /// <summary>
+    /// Event arguments to hold data for participant creation.
+    /// </summary>
     public class NewParticipantEventArgs : EventArgs
     {
         public ParticipantData Data { get; set; }
