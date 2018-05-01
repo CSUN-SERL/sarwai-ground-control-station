d38ac847fd4217bdde961f7388851f35580b76c6
Collin <MaxConers@gmail.com>
2018-04-30 21:21:31 -0700

    changed hardcoded values of gaze partitioning to API's gameObject find method

--- Assets/Scripts/DataCollection/Physiological/Sensors/GazeTracking.cs
+++ Assets/Scripts/DataCollection/Physiological/Sensors/GazeTracking.cs
@@ -5,59 +5,11 @@ using UnityEngine;
 
 namespace Assets.Scripts.DataCollection.Physiological.Sensors
 {
-    public static class AttentionId
+    /// <summary>
+    ///     Gatheres the location of the gaze, specifically, the x and y relative to the screen.
+    /// </summary>
+    public class GazeTracking : PhysiologicalMonoBehaviour<Vector2>
     {
-        public const float Robot1 = 0;
-        public const float Robot2 = 1;
-        public const float Robot3 = 2;
-        public const float Robot4 = 3;
-        public const float QueryList = 4;
-        public const float QueryQuestion = 5;
-        public const float NotificationList = 6;
-
-        /// <summary>
-        ///     Uses hardcoded numbers to find the location of the vectory coresponding to an attention_id.
-        /// </summary>
-        /// <param name="_screenValue"></param>
-        /// <returns></returns>
-        public static float getAttentionId(Vector2 _screenValue)
-        {
-            var half_screen = 980;
-            var right_side = 2100;
-            var attention_id = -1F;
-            if (_screenValue.x > half_screen)
-            {
-                if (_screenValue.y > 550)
-                {
-                    if (_screenValue.x < half_screen + 470)
-                        attention_id = Robot1;
-                    else
-                        attention_id = Robot2;
-                }
-                else
-                {
-                    if (_screenValue.x < half_screen + 470)
-                        attention_id = Robot3;
-                    else
-                        attention_id = Robot4;
-                }
-            }
-            else
-            {
-                if (_screenValue.x > 760)
-                    attention_id = QueryList;
-                else if (_screenValue.x < 280)
-                    attention_id = NotificationList;
-                else
-                    attention_id = QueryQuestion;
-            }
-            return attention_id;
-        }
-    }
-    public class GazeTracking : PhysiologicalMonoBehaviour<Vector3>
-    {
-        
-
         public override IEnumerator Log()
         {
             while (true)
@@ -66,25 +18,36 @@ namespace Assets.Scripts.DataCollection.Physiological.Sensors
                 var gazePoint = TobiiAPI.GetGazePoint();
                 
                 if (gazePoint.IsValid)
-                {
-                    Vector2 _screenValue = gazePoint.Screen;
-                    
-                    _sensorValue = new Vector3(_screenValue.x, _screenValue.y, AttentionId.getAttentionId(_screenValue));
-
-                }
+                    _sensorValue = gazePoint.Screen;
                 else
-                {
                     _sensorValue = GetSensorFailureValue();
-                }
-                    
-                
-               // Debug.Log("sensor value is "+_sensorValue +" resolution: " + Screen.currentResolution);
             }
         }
 
-        public override Vector3 GetSensorFailureValue()
+        public override Vector2 GetSensorFailureValue()
         {
-            return new Vector3(-1F, -1F,-1F);
+            return new Vector2(-1F, -1F);
+        }
+    }
+
+    /// <summary>
+    ///     Gathers the name of a gameObject attached with <see cref="UIGazeAware"/>, based on gaze position relative to screen.
+    /// </summary>
+    public class GazeAttention : PhysiologicalMonoBehaviour<string>
+    {
+        public override string GetSensorFailureValue()
+        {
+            return "none";
+        }
+
+        public override IEnumerator Log()
+        {
+            while(true)
+            {
+                yield return new WaitForSeconds(0F);
+                _sensorValue = TobiiAPI.GetFocusedObject().name.ToString();
+            }
+            
         }
     }
 }
---
+++ Assets/Scripts/DataCollection/Physiological/Sensors/UIGazeAware.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGazeAware : Assets.Tobii.Framework.GazeAware
+{
+    BoxCollider gazeScope;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.AddComponent<BoxCollider>();
+        gazeScope = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var size = gameObject.GetComponent<RectTransform>().rect.size;
+        gazeScope.size = new Vector3(size.x, size.y, 0F);
+    }
+}
